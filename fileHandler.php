a59ba03f460e587664f6772d7692675bcf296692
alfhh <alfhh4@gmail.com>
2015-05-02 19:12:03 -0500

    Read2

--- fileHandler.php
+++ fileHandler.php
@@ -33,7 +33,6 @@ else if ($_FILES['file']['name'] == "")
     // Mostrar datos introducidos
     $Filepath = $nombreDirectorio . $nombreArchivo;
 
-    //echo "no";
 try
 {
     $Spreadsheet = new SpreadsheetReader($Filepath);
@@ -42,16 +41,10 @@ try
     $Sheets = $Spreadsheet -> Sheets();
 
     $tableName;
-    //$nombres = "";
     $nombres = array();
     $size = sizeof($Sheets);
 
-    /*
-     *
-     * $stack = array("orange", "banana");
-array_push($stack, "apple", "raspberry");
-print_r($stack);
-     */
+
     $string = "";
 
     foreach ($Sheets as $Index => $Name) {
@@ -148,20 +141,9 @@ print_r($stack);
                     $i++;
                 }
                 $string.= "), ";
-                /*
-                if($size>($count)) {
-                    $string+= "), ";
-                    $string+= " " . $count . " " . $size . " ";
-                }
-                else {
-                    $string+= ");";
-                    $string+=   " " . $count . " " . $size . " ";
-                }
-                */
 
             }
             $count++;
-            //echo implode("|", $Row);
 
         }
 
@@ -169,106 +151,6 @@ print_r($stack);
         echo $string;
         unlink($Filepath);
     }
-
-/**
- * XLS parsing uses php-excel-reader from http://code.google.com/p/php-excel-reader/
- */
-
-
-	// Excel reader from http://code.google.com/p/php-excel-reader/
-
-	//require('SpreadsheetReader.php');
-
-    // If you need to parse XLS files, include php-excel-reader
-    //$Reader = new SpreadsheetReader('personas.xlsx');
-/*
-    $i = 0;
-    $j = 0;
-    $sql = '';
-    foreach ($Spreadsheet as $Row)
-    {
-       // echo $ow;
-        //adds the sql for creating the table
-        if($j = 0){
-            $sql += 'CREATE TABLE ';
-            $sql += $Row[0];
-            $sql += ' ';
-        }
-        //adds the sql for creating each of the columns
-        else if($j = 1){
-            foreach ($Row as $cName){
-                //exploding the column name and type into an array
-                $cNameArray = explode(',', $cName);
-                //adding the column name
-                $sql += '(';
-                $sql += $cNameArray[0];
-                $sql += ' ';
-                //adding the column type
-                $sql += $cNameArray[1];
-                //adding the size restraint
-                $sql += '(';
-                $sql += $cNameArray[2];
-                $sql += '), ';
-            }
-        }
-        $j += 1;
-        echo $sql;
-    }
-*/
-
-/*
-
-
-    echo '---------------------------------'.PHP_EOL;
-    echo 'Spreadsheets:'.PHP_EOL;
-    print_r($Sheets);
-    echo '---------------------------------'.PHP_EOL;
-    echo '---------------------------------'.PHP_EOL;
-
-    foreach ($Sheets as $Index => $Name)
-    {
-        echo '---------------------------------'.PHP_EOL;
-        echo '*** Sheet '.$Name.' ***'.PHP_EOL;
-        echo '---------------------------------'.PHP_EOL;
-
-        $Time = microtime(true);
-
-        $Spreadsheet -> ChangeSheet($Index);
-
-        foreach ($Spreadsheet as $Key => $Row)
-        {
-            echo $Key.': ';
-            if ($Row)
-            {
-                print_r($Row);
-            }
-            else
-            {
-                var_dump($Row);
-            }
-            $CurrentMem = memory_get_usage();
-
-            echo 'Memory: '.($CurrentMem - $BaseMem).' current, '.$CurrentMem.' base'.PHP_EOL;
-            echo '---------------------------------'.PHP_EOL;
-
-            if ($Key && ($Key % 500 == 0))
-            {
-                echo '---------------------------------'.PHP_EOL;
-                echo 'Time: '.(microtime(true) - $Time);
-                echo '---------------------------------'.PHP_EOL;
-            }
-        }
-
-        echo PHP_EOL.'---------------------------------'.PHP_EOL;
-        echo 'Time: '.(microtime(true) - $Time);
-        echo PHP_EOL;
-
-        echo '---------------------------------'.PHP_EOL;
-        echo '*** End of sheet '.$Name.' ***'.PHP_EOL;
-        echo '---------------------------------'.PHP_EOL;
-
-
-*/
 
 }catch (Exception $E)
 {
